01b829a327b46d36ac2287e1139d81bbe7f02efc
HanmaDaiki <daikihanma@outlook.com>
2023-05-05 16:05:20 +0300

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,14 +1,6 @@
-import {Fragment, useState} from 'react'
-import {Dialog, Disclosure, Popover, Transition} from '@headlessui/react'
-import {
-  Bars3Icon,
-  XMarkIcon,
-} from '@heroicons/react/24/outline'
-import {ChevronDownIcon} from '@heroicons/react/20/solid'
+import {FC} from 'react';
 
-const Header = () => {
-  const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
-
+const Header: FC = () => {
   return (
     <header className="absolute px-10 py-2 top-0 w-full bg-stone-50 border-b border-stone-300 drop-shadow-md">
       <span className="text-indigo-700 text-2xl">Learn More Words</span>
